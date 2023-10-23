e9ca8612c6c601f14b9eecbc02ab3656e613bb39
Mingwei Zhang <digizeph@users.noreply.github.com>
2023-10-23 15:37:31 -0700

    bump up rust-s3 dependency version

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@
 //!
 //! Enable all compression algorithms, and handle remote files (default)
 //! ```toml
-//! oneio = "0.11"
+//! oneio = "0.13"
 //! ```
 //!
 //! Select from supported feature flags
 //! ```toml
-//! oneio = {version = "0.11", features = ["remote", "gz"]}
+//! oneio = {version = "0.13", features = ["remote", "gz"]}
 //! ```
 //!
 //! Supported feature flags:
