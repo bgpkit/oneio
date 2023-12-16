09a169488ae7a12c27f3a809aa2dfb46ea368634
Mingwei Zhang <mingwei@mwzhang.com>
2023-12-15 18:11:43 -0800

    support getting reader directly by s3 url

--- examples/s3_operations.rs
+++ examples/s3_operations.rs
@@ -1,5 +1,6 @@
 use oneio::{s3_copy, s3_delete, s3_download, s3_exists, s3_list, s3_reader, s3_stats, s3_upload};
 use std::io::Read;
+use tracing::info;
 
 /// This example shows how to upload a file to S3 and read it back.
 ///
@@ -9,25 +10,26 @@ use std::io::Read;
 /// - AWS_REGION (e.g. "us-east-1") (use "auto" for Cloudflare R2)
 /// - AWS_ENDPOINT
 fn main() {
-    // upload to S3
+    tracing_subscriber::fmt::init();
+
+    info!("upload to S3");
     s3_upload("oneio-test", "test/README.md", "README.md").unwrap();
 
-    // read directly from S3
+    info!("read directly from S3");
     let mut content = String::new();
     s3_reader("oneio-test", "test/README.md")
         .unwrap()
         .read_to_string(&mut content)
         .unwrap();
-    println!("{}", content);
 
-    // download from S3
+    info!("download from S3");
     s3_download("oneio-test", "test/README.md", "test/README-2.md").unwrap();
 
-    // get S3 file stats
+    info!("get S3 file stats");
     let res = s3_stats("oneio-test", "test/README.md").unwrap();
     dbg!(res);
 
-    // error if file does not exist
+    info!("error if file does not exist");
     let res = s3_stats("oneio-test", "test/README___NON_EXISTS.md");
     assert!(res.is_err());
     assert_eq!(
@@ -36,7 +38,7 @@ fn main() {
     );
     assert_eq!(true, s3_exists("oneio-test", "test/README.md").unwrap());
 
-    // copy S3 file to a different location
+    info!("copy S3 file to a different location");
     let res = s3_copy("oneio-test", "test/README.md", "test/README-temporary.md");
     assert!(res.is_ok());
     assert_eq!(
@@ -44,7 +46,7 @@ fn main() {
         s3_exists("oneio-test", "test/README-temporary.md").unwrap()
     );
 
-    // delete temporary copied S3 file
+    info!("delete temporary copied S3 file");
     let res = s3_delete("oneio-test", "test/README-temporary.md");
     assert!(res.is_ok());
     assert_eq!(
@@ -52,7 +54,19 @@ fn main() {
         s3_exists("oneio-test", "test/README-temporary.md").unwrap()
     );
 
-    // list S3 files
+    info!("list S3 files");
     let res = s3_list("oneio-test", "test/", Some("/")).unwrap();
     dbg!(res);
+
+    info!("read compressed s3 file by url");
+    let mut writer = oneio::get_writer("test/README.md.gz").unwrap();
+    write!(writer, "{}", content).unwrap();
+    drop(writer);
+    s3_upload("oneio-test", "test/README.md.gz", "test/README.md.gz").unwrap();
+    let mut new_content = String::new();
+    oneio::get_reader("s3://oneio-test/test/README.md.gz")
+        .unwrap()
+        .read_to_string(&mut new_content)
+        .unwrap();
+    assert_eq!(content, new_content);
 }
